14cb863bead74c6a2f780fefb16590621d2a6f02
Sergio Vargas <sergiovl219@gmail.com>
2022-11-17 18:05:30 -0600

    feature: code structure fixed in order to be more scalable

---
+++ app/app/core/config.py
@@ -0,0 +1,23 @@
+import secrets
+from typing import Any, Dict, List, Optional, Union
+
+from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, validator
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+
+    # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
+    # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
+    # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    PROJECT_NAME: str
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
