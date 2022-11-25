11a828589376ec5b0e02563ed500fe1e926d89e7
Sergio Vargas <sergiovl219@gmail.com>
2022-11-24 19:29:41 -0600

    feature: Secret key added to env file. feature: dotenv file added to git ignore.

--- app/app/core/config.py
+++ app/app/core/config.py
@@ -7,7 +7,7 @@ from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, v
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
 
-    SECRET_KEY: str = secrets.token_urlsafe(32)
+    SECRET_KEY: str
 
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
