b0ac1523be3e50176e6664943956441ec5e3deaf
Sergio Vargas <sergiovl219@gmail.com>
2022-11-29 19:26:31 -0600

    feature: services methods documented

--- app/app/services/web_scrapping.py
+++ app/app/services/web_scrapping.py
@@ -9,22 +9,47 @@ from app.services.main import AppService
 
 class WebScrappingService(AppService):
     def __init__(self, url: str):
+        """
+        Object initialization with the url to perform the web scrapping.
+        BeautifulSoup and Request are used to get the objects that will be shared in the methods.
+
+        Args:
+         :param url: The URL from where we will obtain the information.
+        """
         super().__init__()
         page = requests.get(url)
         self.soup = BeautifulSoup(page.content, "html.parser")
         self.url = url
 
     def get_title_tag_content(self) -> str:
+        """
+        Method to get the title tag content from a website.
+
+        Returns:
+            Tittle tag content text or default text response.
+        """
         title = self.soup.find('title')
         title_tag = title.string if title else "No title content given"
         return title_tag
 
     def get_meta_description_content(self) -> str:
+        """
+        Method to get the meta description content from a website.
+
+        Returns:
+            Meta description content text or default text response.
+        """
         meta_description = self.soup.find("meta", property="og:description")
         meta_description_content = meta_description["content"] if meta_description else "No meta description given"
         return meta_description_content
 
     def get_url_favicon(self) -> str:
+        """
+        Method to get the URL of the favicon website.
+
+        Returns:
+            Tittle tag content text or default text response.
+        """
         domain = urlparse(self.url).netloc
 
         links = self.soup.find_all(name='link') or []
@@ -41,6 +66,12 @@ class WebScrappingService(AppService):
         return url_favicon
 
     def get_body_first_h1(self) -> str:
+        """
+        Method to get the first h1 tag content from a website.
+
+        Returns:
+            First h1 tag content text or default text response.
+        """
         page_body = self.soup.find('body')
         if page_body:
             first_h1 = page_body.find_next("h1")
@@ -51,6 +82,12 @@ class WebScrappingService(AppService):
         return body_first_h1
 
     def get_web_scrapping_response(self) -> dict:
+        """
+        Method to map the response for the web-scrapping endpoint.
+
+        Returns:
+            Dictionary, mapping all the required fields in WebScrappingResponse schema.
+        """
         return {
             "title_tag": self.get_title_tag_content(),
             "meta_description": self.get_meta_description_content(),
