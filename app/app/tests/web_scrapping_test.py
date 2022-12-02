47dc146d32caa67a4853bf1aea32379b15dd6a52
Sergio Vargas <sergiovl219@gmail.com>
2022-12-01 20:22:15 -0600

    feature: New tests no meta description and no first h1 in body

--- app/app/tests/web_scrapping_test.py
+++ app/app/tests/web_scrapping_test.py
@@ -41,3 +41,43 @@ def test_web_info_keys_validation_data_filled(
     assert content.get("meta_description") is not None
     assert content.get("url_favicon") is not None
     assert content.get("body_first_h1") is not None
+
+
+def test_web_info_no_meta_description_content(
+        client: TestClient
+) -> None:
+    """
+    Test OK, response status 200 and all keys given but no meta description in website.
+    """
+    response = client.get(
+        f"{settings.API_V1_STR}/web-scrapping/",
+        params={"url": "https://python-poetry.org/"}
+    )
+    assert response.status_code == 200
+    content = response.json()
+
+    # Test all data filled in response.
+    assert content.get("title_tag") is not None
+    assert content.get("meta_description") == "No meta description given"
+    assert content.get("url_favicon") is not None
+    assert content.get("body_first_h1") is not None
+
+
+def test_web_info_no_body_first_h1(
+        client: TestClient
+) -> None:
+    """
+    Test OK, response status 200 and all keys given but no h1 tag in body's website.
+    """
+    response = client.get(
+        f"{settings.API_V1_STR}/web-scrapping/",
+        params={"url": "https://python-poetry.org/"}
+    )
+    assert response.status_code == 200
+    content = response.json()
+
+    # Test all data filled in response.
+    assert content.get("title_tag") is not None
+    assert content.get("meta_description") is not None
+    assert content.get("url_favicon") is not None
+    assert content.get("body_first_h1") == "No h1 text given"
